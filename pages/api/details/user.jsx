461747050b368bb3d20f0493290c64b4e304eea6
WOOL DAN <parasharash6@gmail.com>
2023-01-07 17:18:18 +0530

    added baseurl

--- pages/api/details/user.jsx
+++ pages/api/details/user.jsx
@@ -1,16 +1,17 @@
-import User from "../../../model/userprofile";
+import User from "../../../model/user";
 import connectmongo from "../../../utils/connect";
 
 const handler = async(req, res)=> {
-    const { name,email} = req.query;  
+    const { name,email,password} = req.query;  
     await connectmongo();
     const Userdata = new User({
         name: name,
         email: email,
+        password: password,
     })
     const result = await User.insertMany([Userdata]);
-    console.log(result);
-    res.status(200).json({ name,email })
+    // console.log(result);
+    res.status(200).json({ name,email,password })
   }
 
   export default handler;
