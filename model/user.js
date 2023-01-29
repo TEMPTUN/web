bc07a784f2e678904a3dc7e291076f72692c6841
WOOL DAN <parasharash6@gmail.com  git config --global user.name  TEMPTUN>
2023-01-29 18:11:32 +0530

    added little bugs

--- model/user.js
+++ model/user.js
@@ -24,38 +24,11 @@ const user = new Schema({
     PostId:[{type:String,default:[],unique:true}],
     LikeId:[{type:String,default:[],unique:true}],
     DisscussionId:[{type:String,default:[]}],
-    experinceId:[
-        {
-            title:{type:String,default:""},
-            company:{type:String,default:""},
-            employtype:{type:String,default:""},
-            start:{type:String,default:""},
-            end:{type:String,default:""},
-            location:{type:String,default:""},
-        }
-    ],
-    educationId:[
-        {
-            school:{type:String,default:""},
-            degree:{type:String,default:""},
-            start:{type:String,default:""},
-            end:{type:String,default:""},
-            location:{type:String,default:""},
-        }
-    ],
+    experienceId:[{type:String,default:[]}],
+    educationId:[{type:String,default:[]}],
     skillId:[{type:String,default:[]}],
-    projectId:[
-        {
-            title:{type:String,default:""},
-            description:{type:String,default:""},
-            link:{type:String,default:""},
-        }
-    ],
-    linkId:[{
-        github:{type:String,default:""},
-        linkedin:{type:String,default:""},
-        instagram:{type:String,default:""},
-    }],
+    projectId:[{type:String,default:[]}],
+    linkId:[{type:String,default:[]}],
     location:{
         type:String,
     },
