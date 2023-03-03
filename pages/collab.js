b790dda47dd538b853d40268f93900340cf47f61
WOOL DAN <parasharash6@gmail.com  git config --global user.name  TEMPTUN>
2023-03-04 01:55:00 +0530

    resolving error2

--- pages/collab.js
+++ pages/collab.js
@@ -1,31 +1,14 @@
 import React, { useState } from 'react'
-import Group from '../component/CollabPage/Group';
-import Footer from '../component/FooterComp/Footer';
-import style from '../styles/collab.module.scss';
-import Discussion from '../component/CollabPage/Discussion';
+import Top from '../component/Explore/Top';
 
 const collab = () => {
   const[open,setOpen] = useState("Group"); 
   return (
    
-      <div className={style.collabFrame}>
-        <div className={style.switchbutton}>
-          <input className={style.switchbuttoncheckbox} onClick={()=>open==="Group"?setOpen("Discuss"):setOpen("Group")} type="checkbox"></input>
-          <label className={style.switchbuttonlabel} for=""><span   className={style.switchbuttonlabelspan}>Groups</span></label>
-        </div>
-         
-        <div className={style.optionBox}>
-          {open==="Group" &&<Group/>}
-          {open==="Discuss" &&<Discussion/>}
-        </div>
-         
-        <Footer/>
-
-        <styled jsx>{`
-        
-        
-      `}</styled>
-      </div>
+       
+      <>
+        <Top/>
+      </>
    
     
   )
