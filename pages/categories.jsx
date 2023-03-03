192547a7af8028729f2c5181522f5eab6989bfcf
WOOL DAN <parasharash6@gmail.com  git config --global user.name  TEMPTUN>
2023-03-04 01:32:33 +0530

    resolved error

--- pages/categories.jsx
+++ pages/categories.jsx
@@ -1,6 +1,4 @@
 import React, { useEffect } from 'react';
-import { useRouter } from 'next/router'
-import { useSelector } from 'react-redux';
 import Category_selection from "../component/category/Category_selection";
 
 function categories() {
