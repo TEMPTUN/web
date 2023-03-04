85466f5af62f0741faa10db05bc3119e89be0ce5
WOOL DAN <parasharash6@gmail.com  git config --global user.name  TEMPTUN>
2023-03-04 05:54:58 +0530

    changed resolved

--- component/CollabPage/CreateGroup.js
+++ component/CollabPage/CreateGroup.js
@@ -7,6 +7,7 @@ import styled from "styled-components";
 import { useState } from 'react';
 import {category_Data,allCategory} from '../category/category_data';
 import axios from 'axios';
+
 const base_url = process.env.NEXT_PUBLIC_URL;
 
 const CreateGroup = ({setOpen}) => {
